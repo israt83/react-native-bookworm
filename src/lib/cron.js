88a76b0c9428cbdfb53a36892a2c332f6c74adab
Israt Jahan Jisha <isratjahanfci@gmail.com>
2025-10-05 20:44:51 +0600

    add corn commit

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -2,13 +2,22 @@ import cron from 'cron';
 import https from 'https';
 
 const job = new cron.CronJob('*/14 * * * *', () => {
-  https
-  .get(process.env.API_URL, (res) => {
-    if(res.statusCode === 200) console.log('Cron job executed successfully');
-    else console.log('Cron job failed with status code:', res.statusCode);
-    })
-    .on('error', (err) => { console.log('Cron job error:', err.message); });
-    
-  })
+  if (!process.env.API_URL) {
+    console.log('API_URL not defined');
+    return;
+  }
 
-  export default job;
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log('Cron job executed successfully');
+      else console.log('Cron job failed with status code:', res.statusCode);
+    })
+    .on('error', (err) => {
+      console.log('Cron job error:', err.message);
+    });
+});
+
+// Cron job start করা
+job.start();
+
+export default job;
